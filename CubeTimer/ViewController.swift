dcd16a30743066855281bf994ff8a58ce1fa64ed
sparrowapps <mt.sparrow@gmail.com>
2018-01-18 22:33:54 +0900

    animation 기능 추가

--- CubeTimer/ViewController.swift
+++ CubeTimer/ViewController.swift
@@ -10,16 +10,108 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayTimerLabel: UILabel!
+    @IBOutlet var thisView: UIView!
+    @IBOutlet weak var cubeView: UIView!
+    
+    var counter = 0.0 {
+        didSet{
+            if counter >= 3600.0 {
+                counter = 0.0
+            }
+            
+            let min = Int(counter / 60.0)
+            let sec = Int(counter) % 60
+            let dot = Int(counter * 100) % 100
+            displayTimerLabel.text = String(format: "%02d:%02d.%02d", min, sec, dot)
+        }
+    }
+    var timer = Timer()
+    var isPlaying = false {
+        didSet {
+            if isPlaying {
+                animationStart()
+            } else {
+                animationStop()
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture) )
+        longPress.minimumPressDuration = 0.5
+        
+        let taps = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        
+        self.thisView.gestureRecognizers = [longPress,taps]
+        
+        animationStop()
+    }
+    
+    func animationStop() {
+        let image = UIImage(named: "cube_stop")
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 0 , y: 0, width:
+            256.0 , height: 192.0)
+        
+        cubeView.addSubview(imageView)
+    }
+    
+    func animationStart() {
+        //gif 이미지 처리
+        if #available(iOS 9.0, *) {
+            if let asset = NSDataAsset(name: "cube") {
+                let data = asset.data
+                _ = try? JSONSerialization.jsonObject(with: data, options: [])
+                
+                let gifImage = UIImage.gifImageWithData(data)
+                let imageView = UIImageView(image: gifImage)
+                imageView.frame = CGRect(x: 0 , y: 0, width:
+                    256.0 , height: 192.0)
+                
+                cubeView.addSubview(imageView)
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    @objc func handleTapGesture(recoginzer: UILongPressGestureRecognizer) {
+        stop()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func handleLongPressGesture(recoginzer: UILongPressGestureRecognizer) {
+        reset()
     }
 
-
+    func start() {
+        if isPlaying {
+            return
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        
+        isPlaying = true
+    }
+    
+    func stop() {
+        timer.invalidate()
+        isPlaying = false
+    }
+    
+    func reset() {
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+    }
+    
+    @objc func UpdateTimer() {
+        counter = counter + 0.01
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        start()
+    }
 }
-
